287e3e01207a8468d19462c0e7b2a8687386b459
Andrey772005 <45210596+Andrey772005@users.noreply.github.com>
2024-04-07 15:13:38 +0300

    some adaptive menus, add file to git ignore

--- src/components/layout/nav/styles.js
+++ src/components/layout/nav/styles.js
@@ -3,7 +3,7 @@ import {defaultTheme} from "../../../theme/default.js";
 
 export const StyledNav = styled.nav`
   display: grid;
-  grid-template-columns: 110px 110px auto 40px 40px ;
+  grid-template-columns: 1fr 1fr 3fr 1fr 1fr ;
   align-items: center;
   width: auto;
   height: 51px;
--- src/styled/button/styles.js
+++ src/styled/button/styles.js
@@ -6,10 +6,9 @@ export const Button = styled.button`
   font-family: "Roboto", sans-serif;
   text-align: center;
   margin: 12px 0 12px 15px;
-  line-height: 0px;
   height: 28px;
   color: #E20055;
   border: 2px solid #E20055;
   border-radius: 100px;
-  width: 101px
+  width: clamp(101px, 17vw, 300px)
 `
