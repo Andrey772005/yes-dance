a1be984f1edcccfc2a5c18a5e7e3aa1b1be41399
Andrew <as772005@gmail.com>
2024-04-18 19:28:34 +0300

    nav buttons

--- src/components/layout/main-nav/main-nav.jsx
+++ src/components/layout/main-nav/main-nav.jsx
@@ -9,7 +9,10 @@ export default function MainNav() {
       <AboutButton role={"button"}>
         О студии
       </AboutButton>
-      <NavButtonList/> 
+      <NavButtonList/>
+      <AboutButton role={"button"}>
+        О нас
+      </AboutButton> 
     </Navigation>
   )
 }
