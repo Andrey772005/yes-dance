fcfc10f96a20dc1f9233f4ff80919b65147b86f0
Andrew <as772005@gmail.com>
2024-05-13 19:52:36 +0300

    adaptive design

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,14 +1,48 @@
+import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import GlobalStyle from './styles.js';
 import UserNav from '../layout/nav/nav.jsx';
 import Main from "../pages/main/main-page.jsx";
 import './styles.js'
 
+
+const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery ({ minWidth: 320, maxWidth: 498 })
+  return isMobile ? children : null
+};
+
+const Tablet = ({ children }) => {
+  const isTablet = useMediaQuery ({ minWidth: 499, maxWidth: 1199 })
+  return isTablet ? children : null
+};
+
+const Desktop = ({ children }) => {
+  const isDesktop = useMediaQuery ({ minWidth: 1200 })
+  return isDesktop ? children : null
+};
+
+const Adaptive = () => {
+  return (
+    <>
+      <Mobile>
+        <UserNav/>
+        <Main/>
+      </Mobile>
+      <Tablet>
+        Tablet
+      </Tablet>
+      <Desktop>
+        Desktop
+      </Desktop>
+    </>
+  )
+}
+
 function App() {
   return (
     <>
       <GlobalStyle/>
-      <UserNav/>
-      <Main/>
+      <Adaptive/>
     </>
   )
 }
